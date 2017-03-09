882d203f2049400c5609617edaf4971679e56dbe
tictumTarde <tictumTarde@172.16.112.58>
2017-03-09 17:29:12 +0100

    Esqueleto mediocompletado

---
+++ TicInterview/src/main/java/com/tt/ticinterview/beans/from/Type.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tt.ticinterview.beans.from;
+
+/**
+ * @since 09/03/2017
+ * @author tictumTarde
+ * @version 0.1
+ */
+public enum Type {
+    
+    CHECKBOX,TEXT,NUMBER,DATE,RESET,SELECT,
+    OPTION,RADIOBUTTON,RANGE,ULR,VIDEO
+    
+    
+}
