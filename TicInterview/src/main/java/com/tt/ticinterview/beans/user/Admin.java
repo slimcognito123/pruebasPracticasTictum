ffc53b62887ba4a4f0bda1fb5b1a77565b4344f3
tictumTarde <tictumTarde@172.16.112.58>
2017-03-09 15:57:29 +0100

    Primer esqueleto

---
+++ TicInterview/src/main/java/com/tt/ticinterview/beans/user/Admin.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tt.ticinterview.beans.user;
+
+/**
+ * @since 09/03/2017
+ * @author tictumTarde - PNM
+ * @version 0.1
+ */
+public class Admin implements BasicUser{
+    
+}
---
+++ TicInterview/src/main/java/com/tt/ticinterview/beans/video/Video.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tt.ticinterview.beans.video;
+
+/**
+ * @since 09/03/2017
+ * @author tictumTarde -PNM
+ * @version 0.1
+ */
+public interface Video {
+    
+}
