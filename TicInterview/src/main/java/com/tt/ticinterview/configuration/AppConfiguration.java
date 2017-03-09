8d977ddb0e45a009e0d6bb181eb8fcdd5af67d16
tictumTarde <tictumTarde@172.16.112.58>
2017-03-09 15:29:51 +0100

    Primer esqueleto

---
+++ TicInterview/src/main/java/com/tt/ticinterview/configuration/AppConfiguration.java
@@ -0,0 +1,17 @@
+package com.tt.ticinterview.configuration;
+
+import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+
+@Configuration
+@ComponentScan(basePackages="com.tt.mavenproject1")
+@EnableWebMvc
+public class AppConfiguration{
+
+	
+
+	
+}
