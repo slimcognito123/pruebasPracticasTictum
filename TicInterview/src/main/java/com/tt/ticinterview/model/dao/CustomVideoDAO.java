cfcf6e8a7c19d7282738a91268d35f30c9946e02
slimcognito123 <jmmartind01@gmail.com>
2017-03-13 17:58:12 +0100

    DAO 90% aprox

---
+++ TicInterview/src/main/java/com/tt/ticinterview/model/dao/CustomVideoDAO.java
@@ -0,0 +1,32 @@
+package com.tt.ticinterview.model.dao;
+
+import com.tt.ticinterview.beans.video.CustomVideo;
+
+import javax.persistence.Query;
+import java.util.List;
+
+/**
+ * Created by Tictum on 13/03/2017.
+ */
+public class CustomVideoDAO implements VideoDAO<CustomVideo> {
+
+    @Override
+    public CustomVideo getById(Long id) {
+        return entityManager.find(CustomVideo.class, id);
+    }
+
+    @Override
+    public List<CustomVideo> getAll() {
+        String prepareQuery = "select something from CustomVideo something";
+        Query query = entityManager.createQuery(prepareQuery);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<CustomVideo> searchVideo(String nombreVideo) {
+        String sql = "Select cosa from CustomVideo cosa where cosa.name like :nombre";
+        Query query = entityManager.createQuery(sql);
+        query.setParameter("nombre", "%" + nombreVideo + "%");
+        return query.getResultList();
+    }
+}
--- TicInterview/src/main/java/com/tt/ticinterview/model/dao/FormDAO.java
+++ TicInterview/src/main/java/com/tt/ticinterview/model/dao/FormDAO.java
@@ -1,8 +1,22 @@
 package com.tt.ticinterview.model.dao;
 
 import com.tt.ticinterview.beans.form.Form;
+
+import javax.persistence.Query;
+import java.util.List;
+
 /**
  * Created by Tictum on 10/03/2017.
  */
 public class FormDAO implements CrudDAO<Form> {
+    public Form getById(Long id){
+        return entityManager.find(Form.class,id);
+    }
+
+    public List<Form> getAll(){
+
+        String prepareQuery = "select something from Form something";
+        Query query = entityManager.createQuery(prepareQuery);
+        return query.getResultList();
+    }
 }
