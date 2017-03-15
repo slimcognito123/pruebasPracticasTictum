4692d6f3d1b0e8c67cd9ed35d58a6c5aeeb75204
GARBEN800\J <jorge91rb@gmail.com>
2017-03-15 14:58:53 +0100

    answerManager Terminado

--- TicInterview/src/main/java/com/tt/ticinterview/model/manager/AnswerManager.java
+++ TicInterview/src/main/java/com/tt/ticinterview/model/manager/AnswerManager.java
@@ -1,6 +1,7 @@
 package com.tt.ticinterview.model.manager;
 
 import com.tt.ticinterview.beans.Interview.Answer;
+import com.tt.ticinterview.beans.user.Interviewer;
 import com.tt.ticinterview.model.dao.AnswerDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -22,6 +23,38 @@ public class AnswerManager {
     @Autowired
     @Qualifier("AnswerDAO")
     private AnswerDAO answerDAO;
+
+    @Transactional
+    public void save(Answer answer){
+        try{
+            answerDAO.save(answer);
+        }
+        catch (Exception e){
+            System.err.println("Error save: " + e);
+        }
+
+    }
+
+    @Transactional
+    public void delete (Answer answer){
+        try{
+            answerDAO.delete(answer);
+        }
+        catch (Exception e){
+            System.err.println("Error delete: " + e);
+        }
+    }
+
+    @Transactional
+    public Answer getById(long id){
+        try{
+            return answerDAO.getById(id);
+        }
+        catch (Exception e){
+            System.err.println("Error getById: " + e);
+        }
+        return null;
+    }
 
     @Transactional
     public List<Answer> getAll(){
