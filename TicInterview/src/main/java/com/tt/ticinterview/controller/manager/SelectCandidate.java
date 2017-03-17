06ad42b41fff50873c983374b3a7280a7b07e4f3
GARBEN800\J <jorge91rb@gmail.com>
2017-03-17 15:15:39 +0100

    manager controller Candidate

--- TicInterview/src/main/java/com/tt/ticinterview/controller/manager/SelectCandidate.java
+++ TicInterview/src/main/java/com/tt/ticinterview/controller/manager/SelectCandidate.java
@@ -1,6 +1,15 @@
 package com.tt.ticinterview.controller.manager;
 
+import com.tt.ticinterview.beans.Interview.Answer;
+import com.tt.ticinterview.model.manager.AnswerManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @since 09/03/2017
@@ -10,6 +19,52 @@ import org.springframework.stereotype.Controller;
  */
 
 @Controller
+@RequestMapping("/selectCandidate.do")
 public class SelectCandidate {
+
+    @Autowired
+    private AnswerManager answerManager;
+
+    @RequestMapping(method= RequestMethod.GET)
+    public ModelAndView selectCandidateGet(HttpServletRequest request){
+
+        ModelAndView modelo =  new ModelAndView();
+
+        long  idSelectCandidate = Long.parseLong(request.getParameter("idSelectCandidate"));
+
+        List<Answer> listaRespuestas = answerManager.getAll();
+        for (Answer answer : listaRespuestas) {
+            if (answer.getIdCandidate() == idSelectCandidate) {
+                answer.setIsSelected(true);
+                answerManager.modify(answer);
+            }
+        }
+
+        modelo.setViewName("/Manager/PanelManager");
+
+        return  modelo;
+
+    }
+
+    @RequestMapping(method= RequestMethod.POST)
+    public ModelAndView selectCandidatePost(HttpServletRequest request){
+
+        ModelAndView modelo =  new ModelAndView();
+
+        long  idSelectCandidate = Long.parseLong(request.getParameter("idSelectCandidate"));
+
+        List<Answer> listaRespuestas = answerManager.getAll();
+        for (Answer answer : listaRespuestas) {
+            if (answer.getIdCandidate() == idSelectCandidate) {
+                answer.setIsSelected(true);
+                answerManager.modify(answer);
+            }
+        }
+
+        modelo.setViewName("/Manager/PanelManager");
+
+        return  modelo;
+
+    }
 
 }
